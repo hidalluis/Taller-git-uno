f8e5e1dbf160994b6722d21d76ffe1c8e3d8327b
Miguel <mvinan@icloud.com>
2016-04-27 11:56:46 -0500

    commit inicial

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Taller git Uno
+
+remote, local, branch = rama, commits.
+
+* `git init` --> Inicializa el proyecto de git en local
+* `git add` --> agregar los archivos
+* `git log`
+* `git status`
+* `git fetch`
+* `git pull`
+* `git push`
---
+++ js/app.js
@@ -0,0 +1,30 @@
+// var caja = document.getElementById("caja");
+
+// caja.addEventListener("click", function alejo(){
+//   caja.innerHTML = 'hola'
+// })
+
+// var nombre = "Cadena";
+// var boleanos = true;
+// var enteros = 9;
+
+// function alejandro(){
+//   return "Hola alejandro";
+// }
+
+// console.log(caja)
+
+//Objetos
+
+var objeto = {
+  clave: true
+}
+
+
+
+$('.segundaclase').click(function(){
+  $('#caja').css({ 
+    "background": "red",
+    "height" : "200"
+  });
+})
